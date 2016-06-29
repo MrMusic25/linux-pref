dc9c0286c5384709ad3857a6d3a7b67579660961
Kyle Krattiger <mr.music25@gmail.com>
2016-06-28 17:49:19 -0700

    Added logging and tested script with apt-get. Works!

--- programInstaller.sh
+++ programInstaller.sh
@@ -10,42 +10,51 @@
 
 file="programs.txt"
 program="NULL"
+log="pm.log" # Remember to change this to 'install-logs/pm.log' when other scripts ready
 
 ### Functions
 
 function determinePM() {
-if [[ $(which apt-get) == 0 ]]; then # Most common, so it goes first
-	program="apt"
+#echo "Finding which package manager is in use..."
+if [[ ! -z $(which apt-get) ]]; then # Most common, so it goes first
+	export program="apt"
 	apt-get update
-elif [[ $(which yum) == 0 ]]; then
-	program="yum"
+elif [[ ! -z $(which yum) ]]; then
+	export program="yum"
 	#yum update
-elif [[ $(which yast) == 0 ]]; then
-	program="yast"
-elif [[ $(which pacman) == 0 ]]; then
-	program="pacman"
-elif [[ $(which aptitude) == 0 ]]; then # Just in case apt-get is somehow not installed with aptitude, happens
-	program="aptitude"
+elif [[ ! -z $(which rpm) ]]; then
+	export program="rpm"
+	rpm -F --justdb # Only updates the DB, not the system
+elif [[ ! -z $(which yast) ]]; then
+	export program="yast"
+elif [[ ! -z $(which pacman) ]]; then
+	export program="pacman"
+elif [[ ! -z $(which aptitude) ]]; then # Just in case apt-get is somehow not installed with aptitude, happens
+	export program="aptitude"
 	aptitude update
 fi
 }
 
 function install() {
+printf "\nInstalling $*\n" >> "$log"
 case $program in
 	apt)
-	apt-get install -y $*
+	apt-get install -y $* >> "$log" 
 	;;
 	yum)
-	yum install $*
+	yum install $* >> "$log"
 	;;
 	yast)
-	yast -i $*
+	yast -i $* >> "$log"
+	;;
+	rpm)
+	rpm -i $* >> "$log"
 	;;
 	pacman)
-	pacman -Syu $*
+	pacman -Syu $* >> "$log"
 	;;
 	aptitude)
-	aptitude install -y $*
+	aptitude install -y $* >> "$log"
 	;;
 	*)
 	echo "Package manager not found! Please update script or diagnose problem!"
@@ -58,27 +67,30 @@ esac
 ### Main script
 
 # First, check to see is user is root/sudo. Makes scripting easier
-
 if [ "$EUID" -ne 0 ]; then
 	echo "This script require root privileges, please run as root or sudo!"
 	exit 2
 fi
 
 # Checks for argument and sets as file location
-
 if [[ $# != 0 ]]; then
 	file=$1
 fi
 
 
 # Test to make sure file valid
-
 if [[ ! -e $file ]]; then
 	echo "$file could not be found! Please check and re-run script!"
 	exit 1
 fi
 
+# Now that file is valid, determine program to use
+determinePM
+echo "This distribution is using $program as it's package manager!"
+echo "Now installing programs from $file!"
+
 # Now we can install everything
+touch $log # Touch logfile so nothing fails
 while read -r line; do
 	[[ $line = \#* ]] && continue # Skips comment lines
 	install $line
