ba86e4f5a26ab7f5ec1a7b02d2e6e30cbb98c85d
Kyle Krattiger <mr.music25@gmail.com>
2016-10-13 13:08:14 -0700

    pm.sh can now install from files and folder of files, so now update.sh and programInstaller.sh can be removed

--- packageManager.sh
+++ packageManager.sh
@@ -3,6 +3,9 @@
 # packageManager.sh, a.k.a pm - A universal package manager script
 #
 # Changes:
+# v1.1.0
+# - Install mode now works with files and folders of files, like programInstaller.sh
+#
 # v1.0.1
 # - Some functions didn't work, fixed it with shift statements
 #
@@ -35,13 +38,14 @@
 # - If PM is Arch, make sure it is NOT running as sudo. Otherwise, checkPrivilege "quit"
 #
 #
-# v1.0.1, 13 Oct. 2016 12:36 PST
+# v1.1.0, 13 Oct. 2016 13:05 PST
 
 ### Variables
 
 pmOptions="" # Options to be added when running package manager
 #runMode="NULL"
 program="NULL" # Uninitialized variables are unhappy variables
+programMode="name"
 
 ### Functions
 
@@ -66,7 +70,7 @@ Note: For mode, you can use full names listed below, or single letter in bracket
 Run Modes:
    Re[f]resh                                 : Update the list of available packages and updates from package maintainer
    [U]pgrade                                 : Refresh the package list, and then install all available upgrades
-   [I]nstall <package_1> [package_2] ...     : Attempt to install all given packages
+   [I]nstall <package_1> [package_2] ...     : Attempt to install all given packages. Works with text files and directories as well!
    [R]emove <package_1> [package_2] ...      : Remove given installed packages
    [Q]uery <package_1> [package_2] ...       : Search package databases for matching package names (does not install)
    [P]kginfo <package_1> [package_2] ...     : Display detailed info (dependencies, version, etc) about packages
@@ -113,7 +117,7 @@ function processArgs() {
 			shift
 			for prog in "$@"
 			do
-				universalInstaller "$1"
+				programInstaller "$1"
 				shift
 			done
 			;;
@@ -144,6 +148,73 @@ function processArgs() {
 		esac
 		shift
 	done
+}
+
+function programInstaller() {
+	# Decide on mode
+	if [[ -f $1 ]]; then
+		debug "$1 is a file, running in file mode!"
+		export file=$1
+		export programMode="file"
+	elif [[ -d $1 ]]; then
+		debug "$1 is a folder, running in directory mode!"
+		export file=$1
+		export programMode="directory"
+	else
+		debug "Argument is not a file or folder, assuming $1 is a program!"
+	fi
+	
+	# Now, install based on mode. Taken from the old programInstaller.sh
+	case $programMode in
+		file)
+		announce "Now installing programs listed in $file!" "This may take a while depending on number of updates and internet speed" "Check $logFile for details"
+		while read -r line; do
+			[[ $line = \#* ]] && continue # Skips comment lines
+			universalInstaller "$line"
+		done <$file
+		;;
+		directory)
+		announce "Installing all directories from $file!" "This WILL take a long time!" "Don't go anywhere, you will be asked if each section should be installed!"
+		cd "$file"
+		for list in *.txt;
+		do
+			debug "Asking user if they would like to install $list"
+			getUserAnswer "Would you like to install the programs listed in $list?"
+			if [[ $? -eq 1 ]]; then
+				debug "Skipping $list at user's choice..."
+			else
+				# Ask if user wants to edit file before installing
+				getUserAnswer "Would you like to edit $list before installing?"
+				case $? in
+				0)
+				editTextFile "$list"
+				;;
+				1)
+				debug "User chose not to edit $list"
+				;;
+				*)
+				debug "Unknown option: $?"
+				announce "Error occurred! Please consult log!"
+				exit 1
+				;;
+				esac
+
+				while read -r line; do
+					[[ $line = \#* ]] && continue # Skips comment lines
+					universalInstaller "$line"
+				done <"$list"
+			fi
+		done
+		[[ ! -z $OLDPWD ]] && cd "$OLDPWD" || cd .. # Return to previous location so other scripts don't break
+		;;
+		name)
+		universalInstaller "$1"
+		;;
+		*)
+		debug "Everything is broken. Why. At least you have unique debug messages for an easy CTRL+F."
+		exit 72
+		;;
+	esac
 }
 
 ### Main Script
