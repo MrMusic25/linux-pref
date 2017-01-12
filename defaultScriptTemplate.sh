ae06222533c5e82d7ad18412ddf165fa8c259347
Kyle Krattiger <KyleKrattiger>
2017-01-11 16:32:22 -0800

    Added a default displayHelp to defaultScriptTemplate.sh. Up to user to put in call, or just delete function if script is headless

--- defaultScriptTemplate.sh
+++ defaultScriptTemplate.sh
@@ -31,6 +31,27 @@ else
 	exit 1
 fi
 
+function displayHelp() {
+read -d '' helpVar <<"endHelp"
+
+scriptName.sh - A script to perform a function or fulfill a purpose
+Be sure to look at all the options below
+
+Usage: ./scriptName.sh [options] <required_argument> <also_required> [optional_argument]
+
+Options:
+-o | --option                       : Option with an alias (default)
+--output-only                       : Option with no shotrened name
+-i | --include <file1> [file2] ...  : Option including possibly more than one argument
+-a | --assume <[Y]es or [N]o>       : Option that supports full or shortened argument names
+-v | --verbose                      : Prints verbose debug information. MUST be the first argument!
+
+Put definitions, examples, and expected outcome here
+
+endHelp
+echo "$helpVar"
+}
+
 ### Main Script
 
 
